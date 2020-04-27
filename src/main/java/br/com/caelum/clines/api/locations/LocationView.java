9ec2a36ea9f3eb5177cb467857f1e97b5388e606
Lucas Carol <lucas.carol@thoughtworks.com>
2020-04-27 16:48:27 -0300

    Flavia/Lucas add location mapper test

--- src/main/java/br/com/caelum/clines/api/locations/LocationView.java
+++ src/main/java/br/com/caelum/clines/api/locations/LocationView.java
@@ -1,12 +1,10 @@
-package br.com.caelum.clines.api.airports;
+package br.com.caelum.clines.api.locations;
 
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-@EqualsAndHashCode
 public class LocationView {
     private String country;
     private String state;
