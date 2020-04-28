cb18e16d3a7992ba775541978203a587f1c99d26
Alexandre <alexandre.mello@thoughtworks.com>
2020-04-28 18:09:15 -0300

    Fixing Location Controller

--- src/main/java/br/com/caelum/clines/api/locations/LocationController.java
+++ src/main/java/br/com/caelum/clines/api/locations/LocationController.java
@@ -2,21 +2,25 @@ package br.com.caelum.clines.api.locations;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 import static org.springframework.http.ResponseEntity.created;
 
 @RestController
-@RequestMapping("locations")
+@RequestMapping("location")
 @AllArgsConstructor
 public class LocationController {
 
     private final LocationService service;
 
-    public ResponseEntity<?> createBy(LocationForm form) {
+    @PostMapping
+    public ResponseEntity<?> createBy(@RequestBody @Valid LocationForm form) {
         Long code = service.createLocationBy(form);
 
         URI uri = URI.create("/location/")
