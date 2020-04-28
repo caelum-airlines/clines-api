c048ec3761b67aa6da74feeb9c31b165eb61a2c4
Maria Luisa <maria.luisa@thoughtworks.com>
2020-04-28 10:32:22 -0300

    Fixes url request of AircraftModel

--- src/main/java/br/com/caelum/clines/api/aircraftmodels/AircraftModelController.java
+++ src/main/java/br/com/caelum/clines/api/aircraftmodels/AircraftModelController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("aircraft")
+@RequestMapping("aircraftmodel")
 @AllArgsConstructor
 public class AircraftModelController {
 
