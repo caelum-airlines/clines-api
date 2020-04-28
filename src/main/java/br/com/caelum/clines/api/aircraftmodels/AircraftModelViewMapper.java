76026ba2c5535448175a3b0276489150cb2b226c
Maria Luisa <maria.luisa@thoughtworks.com>
2020-04-28 10:44:56 -0300

    Insert @Component in AircraftModelViewMapper

--- src/main/java/br/com/caelum/clines/api/aircraftmodels/AircraftModelViewMapper.java
+++ src/main/java/br/com/caelum/clines/api/aircraftmodels/AircraftModelViewMapper.java
@@ -2,7 +2,9 @@ package br.com.caelum.clines.api.aircraftmodels;
 
 import br.com.caelum.clines.shared.domain.AircraftModel;
 import br.com.caelum.clines.shared.infra.Mapper;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AircraftModelViewMapper implements Mapper<AircraftModel, AircraftModelView> {
 
     public AircraftModelView map(AircraftModel source) {
