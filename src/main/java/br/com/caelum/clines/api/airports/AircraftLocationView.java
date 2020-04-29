6437919420134051cc751476e1e91f182921d83b
Fernando <fwfurtado@gmail.com>
2020-04-29 07:34:17 -0300

    rename conflicted class and add integration/e2e tests

--- src/main/java/br/com/caelum/clines/api/airports/AircraftLocationView.java
+++ src/main/java/br/com/caelum/clines/api/airports/AircraftLocationView.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 @EqualsAndHashCode
-public class LocationView {
+public class AircraftLocationView {
     private String country;
     private String state;
     private String city;
--- src/test/java/br/com/caelum/clines/api/locations/AircraftLocationViewMapperTest.java
+++ src/test/java/br/com/caelum/clines/api/locations/AircraftLocationViewMapperTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-class LocationViewMapperTest {
+class AircraftLocationViewMapperTest {
     @Test
     public void shouldReturnLocationView() {
         var location = new Location(Country.BR, "São Paulo", "São Paulo");
