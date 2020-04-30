ad502ea3898ca35564bb171c9d61f6c21b2ca4ec
Alexandre <alexandre.mello@thoughtworks.com>
2020-04-30 16:33:39 -0300

    Lucas|Alexandre add promotional code service

--- src/main/java/br/com/caelum/clines/api/promotionalcodes/PromotionalCodeRepository.java
+++ src/main/java/br/com/caelum/clines/api/promotionalcodes/PromotionalCodeRepository.java
@@ -3,6 +3,10 @@ package br.com.caelum.clines.api.promotionalcodes;
 import br.com.caelum.clines.shared.domain.PromotionalCode;
 import org.springframework.data.repository.Repository;
 
+import java.util.Optional;
+
 public interface PromotionalCodeRepository extends Repository<PromotionalCode, Long> {
-    void save(PromotionalCode promotionalCode);
+    PromotionalCode save(PromotionalCode promotionalCode);
+
+    Optional<PromotionalCode> findByCode(String code);
 }
