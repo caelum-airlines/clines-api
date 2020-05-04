cb2d449145618e9c14104b39a611dd25e997de68
Alexandre <alexandre.mello@thoughtworks.com>
2020-05-04 16:33:45 -0300

    Alexandre|Lucas adding promotional code form mapper with id

--- src/test/java/br/com/caelum/clines/api/promotionalcodes/PromotionalCodeFormMapperTest.java
+++ src/test/java/br/com/caelum/clines/api/promotionalcodes/PromotionalCodeFormMapperTest.java
@@ -7,13 +7,29 @@ import static org.hamcrest.Matchers.equalTo;
 
 class PromotionalCodeFormMapperTest {
     @Test
-    void shouldReturnPromotionalCode() {
+    void map_returnNewPromotionalCode() {
         var form = new PromotionalCodeBuilder().getForm();
 
         var mapper = new PromotionalCodeFormMapper();
 
         var promotionalCode = mapper.map(form);
 
+        assertThat(promotionalCode.getCode(), equalTo(form.getCode()));
+        assertThat(promotionalCode.getStartDate(), equalTo(form.getStartDate()));
+        assertThat(promotionalCode.getExpirationDate(), equalTo(form.getExpirationDate()));
+        assertThat(promotionalCode.getDescription(), equalTo(form.getDescription()));
+        assertThat(promotionalCode.getDiscount(), equalTo(form.getDiscount()));
+    }
+
+    @Test
+    void map_returnPromotionalCodeWithId() {
+        var form = new PromotionalCodeBuilder().getForm();
+
+        var mapper = new PromotionalCodeFormMapper();
+
+        var promotionalCode = mapper.map(1L, form);
+
+        assertThat(promotionalCode.getId(), equalTo(1L));
         assertThat(promotionalCode.getCode(), equalTo(form.getCode()));
         assertThat(promotionalCode.getStartDate(), equalTo(form.getStartDate()));
         assertThat(promotionalCode.getExpirationDate(), equalTo(form.getExpirationDate()));
